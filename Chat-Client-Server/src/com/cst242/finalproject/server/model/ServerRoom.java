c0ad9a8329ab92b8c2e034cbf34885bd95d99289
James DeCarlo <James.DeCarlo.Sr@gmail.com>
2014-11-22 23:00:27 -0500

    Worked on the ServerRoom and ServerRoomThread classes

--- Chat-Client-Server/src/com/cst242/finalproject/server/model/ServerRoom.java
+++ Chat-Client-Server/src/com/cst242/finalproject/server/model/ServerRoom.java
@@ -1,25 +1,62 @@
 package com.cst242.finalproject.server.model;
 
 import com.cst242.finalproject.server.viewcontroller.PrimaryWindow;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  *
  * @author James DeCarlo
  */
 public class ServerRoom {    
+    private final static int MAX_USERS = 25;
+    private final static ServerRoomThread[] roomThreads = new ServerRoomThread[MAX_USERS];
+    
     private String roomName;
     private int port;    
         
     private ServerSocket serverSocket;
     private Socket clientSocket;
 
+    @SuppressWarnings("CallToThreadStartDuringObjectConstruction")
     public ServerRoom(String roomName, int port, PrimaryWindow window) {
         
         this.roomName = roomName;
         this.port = port;
                 
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException ex) {
+            window.appendLog("Error creating %s Room: %s%n", roomName, new Date());
+            return;
+        }
+        
+        for(;;){
+            try{
+                this.clientSocket = this.serverSocket.accept();
+                int i;
+                for(i = 0; i < MAX_USERS; i++){
+                    if(ServerRoom.roomThreads[i] == null){
+                        roomThreads[i] = new ServerRoomThread(window, clientSocket, roomThreads);
+                        roomThreads[i].start();
+                        break;
+                    }
+                }
+                if(i == MAX_USERS){
+                    window.appendLog("Room %s has reached its capacity of %d: %s%n", this.roomName, MAX_USERS, new Date());
+                    
+                    // Room is filled close connection to client
+                    this.clientSocket.close();                    
+                }
+                
+                
+            } catch (IOException e){
+                window.appendLog("Failed to connect client to room: %s%n", new Date());
+            }
+        }
+        
     }
 
     public String getRoomName() {
---
+++ Chat-Client-Server/src/com/cst242/finalproject/server/model/ServerRoomThread.java
@@ -0,0 +1,93 @@
+package com.cst242.finalproject.server.model;
+
+import com.cst242.finalproject.server.viewcontroller.PrimaryWindow;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author James DeCarlo
+ */
+public class ServerRoomThread extends Thread {
+
+    private final PrimaryWindow window;
+
+    private final Socket clientSocket;
+    private DataInputStream fromClient;
+    private DataOutputStream toClient;
+
+    private final ServerRoomThread[] roomThreads;
+    private final int MAX_USERS;
+
+    public ServerRoomThread(PrimaryWindow window, Socket clientSocket, ServerRoomThread[] roomThreads) {
+        this.window = window;
+        this.clientSocket = clientSocket;
+        this.roomThreads = roomThreads;
+        this.MAX_USERS = roomThreads.length;
+    }
+
+    @Override
+    public void run() {
+        try {
+            fromClient = new DataInputStream(clientSocket.getInputStream());
+            toClient = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            
+            // remove thread from list and close streams
+            synchronized (this) {
+                for (int i = 0; i < roomThreads.length; i++) {
+                    if (roomThreads[i] == this) {
+                        roomThreads[i] = null;
+                    }
+                }
+            }
+            
+            window.appendLog("Failed to create client data streams in chat room: %s", new Date());
+            try {
+                clientSocket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ServerRoomThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return;
+        }
+
+        // loop and wait for message then broadcast to clients in all threads
+        try {
+            for (;;) {
+                String msg = fromClient.readUTF();
+                
+                synchronized(this){
+                    for(int i = 0; i < roomThreads.length; i++){
+                        if(roomThreads[i] != null){
+                            roomThreads[i].toClient.writeUTF(msg);
+                        }
+                    }
+                }
+                
+            }
+        } catch (IOException ex) {
+            // remove thread from list and close streams
+            synchronized (this) {
+                for (int i = 0; i < roomThreads.length; i++) {
+                    if (roomThreads[i] == this) {
+                        roomThreads[i] = null;
+                    }
+                }
+            }
+            try {
+                this.fromClient.close();
+                this.toClient.close();
+                this.clientSocket.close();
+            } catch (IOException e) {
+                // Do nothing here
+            }
+        }
+
+    }
+
+}
