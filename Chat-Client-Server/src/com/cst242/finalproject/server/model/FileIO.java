6819a796ef88c980d5e20a2b721c0a41653c52ec
Evan-E <dudeman43094@gmail.com>
2014-12-01 12:00:32 -0500

    test commit made change to the FILE IO

--- Chat-Client-Server/src/com/cst242/finalproject/server/model/FileIO.java
+++ Chat-Client-Server/src/com/cst242/finalproject/server/model/FileIO.java
@@ -1,15 +1,57 @@
 package com.cst242.finalproject.server.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
- * This class implements the {@code FileIOInterface} refer to that for 
+ * This class implements the {@code FileIOInterface} refer to that for
  * documentation.
- * 
+ *
  * @author James DeCarlo
  */
-public class FileIO implements FileIOInterface{
+public class FileIO implements FileIOInterface {
 
     @Override
     public boolean register(String loginId, int hashedPassword, String firstName, String lastName, String screenName) {
+        File file = new File("userlist.dat");
+        if (file.exists()) {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                
+                
+                int accountNumber = 1000;
+                
+                while (objectInputStream.available() > 0) {
+                    User user = (User) objectInputStream.readObject();
+                    if (user.getAccountNumber() > accountNumber) {
+                        accountNumber = user.getAccountNumber();
+                    }  
+                }
+                
+                accountNumber++;
+                User user = new User(accountNumber, loginId, hashedPassword, firstName, lastName, screenName);
+                
+                //Writing the User object to the file.
+                objectOutputStream.writeObject(user);
+                
+                fileInputStream.close();
+                objectInputStream.close();
+                fileOutputStream.close();
+                objectOutputStream.close();
+            }
+            catch(Exception e){
+                return false;
+            }
+
+        }
+
         return true;
     }
 
@@ -20,8 +62,8 @@ public class FileIO implements FileIOInterface{
 
     @Override
     public User loginUser(String loginId, int hashedPassword) {
-        User user = new User(1001, "JohnDoe",6969,"John", "Doe", "SomeGuy");
-        
+        User user = new User(1001, "JohnDoe", 6969, "John", "Doe", "SomeGuy");
+
         return user;
     }
 
@@ -29,5 +71,5 @@ public class FileIO implements FileIOInterface{
     public boolean updateUser(int accountNumber, int hashedPassword, String firstName, String lastName, String screenName) {
         return false;
     }
-    
+
 }
