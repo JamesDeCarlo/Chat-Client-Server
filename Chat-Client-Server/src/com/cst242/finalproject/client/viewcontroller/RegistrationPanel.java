9de94d95a9d9c91658b4cc50964c137e83c0eedc
James DeCarlo <James.DeCarlo.Sr@gmail.com>
2014-12-03 14:18:17 -0500

    Worked on java doc

--- Chat-Client-Server/src/com/cst242/finalproject/client/viewcontroller/RegistrationPanel.java
+++ Chat-Client-Server/src/com/cst242/finalproject/client/viewcontroller/RegistrationPanel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cst242.finalproject.client.viewcontroller;
 
 import javax.swing.JButton;
@@ -10,7 +5,8 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 /**
- *
+ * This class is for the registration form panel.
+ * 
  * @author James DeCarlo
  */
 public class RegistrationPanel extends javax.swing.JPanel {
@@ -22,34 +18,66 @@ public class RegistrationPanel extends javax.swing.JPanel {
         initComponents();
     }
 
+    /**
+     * Gets the cancel button.
+     * @return The cancel button.
+     */
     public JButton getBtnCancel() {
         return btnCancel;
     }
 
+    /**
+     * Gets the submit button.
+     * @return The submit button.
+     */
     public JButton getBtnSubmit() {
         return btnSubmit;
     }
 
+    /**
+     * Gets the password confirm text field.
+     * @return The password confirm text field.
+     */
     public JPasswordField getTxtConfirm() {
         return txtConfirm;
     }
 
+    /**
+     * Gets the first name text field.
+     * @return The first name text field.
+     */
     public JTextField getTxtFirstName() {
         return txtFirstName;
     }
 
+    /**
+     * Gets the last name text field.
+     * @return The last name text field.
+     */
     public JTextField getTxtLastName() {
         return txtLastName;
     }
 
+    /**
+     * Gets the login id text field.
+     * @return The login id text field.
+     */
     public JTextField getTxtLoginId() {
         return txtLoginId;
     }
 
+    /**
+     * Gets the password text field.
+     * @return The password text field.
+     */
     public JPasswordField getTxtPassword() {
         return txtPassword;
     }
 
+    /**
+     * Gets the screen name text field.
+     * @return The screen name text field.
+     */
     public JTextField getTxtScreenName() {
         return txtScreenName;
     }
