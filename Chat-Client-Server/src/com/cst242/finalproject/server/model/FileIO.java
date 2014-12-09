4da74fd5d5e07b5acce10665717edd5416ac3ea7
Evan-E <dudeman43094@gmail.com>
2014-12-09 10:51:49 -0500

    unused imports cleaned up

--- Chat-Client-Server/src/com/cst242/finalproject/server/model/FileIO.java
+++ Chat-Client-Server/src/com/cst242/finalproject/server/model/FileIO.java
@@ -1,20 +1,13 @@
 package com.cst242.finalproject.server.model;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * This class implements the {@code FileIOInterface} refer to that for
