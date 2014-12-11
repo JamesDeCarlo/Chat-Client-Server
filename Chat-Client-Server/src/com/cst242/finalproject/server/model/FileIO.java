798b164b0b15be0022d8062595c13732470d2e96
Evan-E <dudeman43094@gmail.com>
2014-12-11 10:43:03 -0500

    Synchronized file IO methods and partial update method

--- Chat-Client-Server/src/com/cst242/finalproject/server/model/FileIO.java
+++ Chat-Client-Server/src/com/cst242/finalproject/server/model/FileIO.java
@@ -24,147 +24,203 @@ public class FileIO implements FileIOInterface {
 
     @Override
     public boolean register(String loginId, int hashedPassword, String firstName, String lastName, String screenName) {
+        synchronized (this) {
+            File file = new File("userlist.dat");
 
-        File file = new File("userlist.dat");
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException ex) {
-                return false;
-            }
-        }
-
-        if (file.length() == 0) {
-            try {
-                writer = new PrintWriter(file);
-
-                accountNumber = 1001;
-
-                User user = new User(accountNumber, loginId, hashedPassword, firstName, lastName, screenName);
-
-                //Writing the User object to the file.
-                writer.println(user.toString());
-
-                writer.close();
-
-            } catch (IOException e) {
-                return false;
-            }
-
-        } else {
-            try {
-                reader = new BufferedReader(new FileReader(file));
-            } catch (IOException e) {
-                return false;
-            }
-
-            List<User> users = new ArrayList<>();
-
-            String line = "";
-            try {
-                while ((line = reader.readLine()) != null) {
-                    if (!line.equals("")) {
-                        User u = new User(line.trim());
-                        users.add(u);
-                    }
-                }
-                reader.close();
-            } catch (IOException ex) {
-                return false;
-            }
-
-            // check if login id is unique and find max account number index
-            accountNumber = 1000;
-
-            for (User u : users) {                
-                if (u.getAccountNumber() > accountNumber) {
-                    accountNumber = u.getAccountNumber();
-                }
-                if (u.getLoginId().toLowerCase().equals(loginId.toLowerCase())) {
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException ex) {
                     return false;
                 }
             }
 
-            // all went well create user and add to list array
-            accountNumber++;
-            User user = new User(accountNumber, loginId, hashedPassword, firstName, lastName, screenName);
-            users.add(user);
+            if (file.length() == 0) {
+                try {
+                    writer = new PrintWriter(file);
 
-            try {
-                writer = new PrintWriter(file);
-            } catch (FileNotFoundException ex) {
-                return false;
-            }
+                    accountNumber = 1001;
 
-            for (User u : users) {                
-                writer.append(u.toString());
-                writer.flush();
+                    User user = new User(accountNumber, loginId, hashedPassword, firstName, lastName, screenName);
+
+                    //Writing the User object to the file.
+                    writer.println(user.toString());
+
+                    writer.close();
+
+                } catch (IOException e) {
+                    return false;
+                }
+
+            } else {
+                try {
+                    reader = new BufferedReader(new FileReader(file));
+                } catch (IOException e) {
+                    return false;
+                }
+
+                List<User> users = new ArrayList<>();
+
+                String line = "";
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        if (!line.equals("")) {
+                            User u = new User(line.trim());
+                            users.add(u);
+                        }
+                    }
+                    reader.close();
+                } catch (IOException ex) {
+                    return false;
+                }
+
+                // check if login id is unique and find max account number index
+                accountNumber = 1000;
+
+                for (User u : users) {
+                    if (u.getAccountNumber() > accountNumber) {
+                        accountNumber = u.getAccountNumber();
+                    }
+                    if (u.getLoginId().toLowerCase().equals(loginId.toLowerCase())) {
+                        return false;
+                    }
+                }
+
+                // all went well create user and add to list array
+                accountNumber++;
+                User user = new User(accountNumber, loginId, hashedPassword, firstName, lastName, screenName);
+                users.add(user);
+
+                try {
+                    writer = new PrintWriter(file);
+                } catch (FileNotFoundException ex) {
+                    return false;
+                }
+
+                for (User u : users) {
+                    writer.append(u.toString());
+                    writer.flush();
+                }
+
+                writer.close();
+
             }
-                        
-            writer.close();
-            
+            return true;
         }
-        return true;
     }
 
     @Override
-    public User loginUser(String loginId, int hashedPassword
-    ) {
-        User loginUser;
-        
-        File file = new File("userList.dat");
-        
-        if(!file.exists()){
-            return null;
-        }
-        
-        if(file.length() == 0){
-            return null;
-        }
-        
-        else{
-            try{
-                reader = new BufferedReader(new FileReader(file));
-                System.out.println("Stream opened");
-            }catch(IOException e){
-                e.printStackTrace();
+    public User loginUser(String loginId, int hashedPassword) {
+        synchronized (this) {
+            User loginUser;
+
+            File file = new File("userList.dat");
+
+            if (!file.exists()) {
+                return null;
             }
-            
-            List<User> users = new ArrayList<>();
-            
-            String line = "";
-            
-            try{
-                while((line = reader.readLine()) != null){
-                    if(!line.equals("")){
-                        User u = new User(line.trim());
-                        users.add(u);
+
+            if (file.length() == 0) {
+                return null;
+            } else {
+                try {
+                    reader = new BufferedReader(new FileReader(file));
+                    System.out.println("Stream opened");
+                } catch (IOException e) {
+                    return null;
+                }
+
+                List<User> users = new ArrayList<>();
+
+                String line = "";
+
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        if (!line.equals("")) {
+                            User u = new User(line.trim());
+                            users.add(u);
+                        }
+
                     }
-                    
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                reader.close();
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-            
-            
-            for(User u : users){
-                if(u.getLoginId().toLowerCase().equals(loginId.toLowerCase()) &&
-                        u.getHashedPassword() == hashedPassword){
-                    return u;
+
+                for (User u : users) {
+                    if (u.getLoginId().toLowerCase().equals(loginId.toLowerCase())
+                            && u.getHashedPassword() == hashedPassword) {
+                        return u;
+                    }
+
                 }
-               
             }
+
+            return null;
         }
-    
-        return null;
     }
 
     @Override
     public boolean updateUser(int accountNumber, int hashedPassword, String firstName, String lastName, String screenName
     ) {
-        return false;
+        synchronized (this) {
+
+            File file = new File("userList.dat");
+
+            if (!file.exists()) {
+                return false;
+            }
+
+            if (file.length() == 0) {
+                return false;
+            } else {
+                try {
+                    reader = new BufferedReader(new FileReader(file));
+                    System.out.println("Stream opened");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                List<User> users = new ArrayList<>();
+
+                String line = "";
+
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        if (!line.equals("")) {
+                            User u = new User(line.trim());
+                            users.add(u);
+                        }
+
+                    }
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                for (User u : users) {
+                    if (u.getAccountNumber() == accountNumber) {
+                        u.setFirstName(firstName);
+                        u.setLastName(lastName);
+                        u.setHashedPassword(hashedPassword);
+                        u.setScreenName(screenName);
+                    }
+                }
+
+                try {
+                    writer = new PrintWriter(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                for (User u : users) {
+                    writer.append(u.toString());
+                    writer.flush();
+                }
+            }
+
+            return false;
+        }
     }
 
 }
