89692fd3281ae0bc8f9501f3f6a8a18e32b92f86
James <James@James-PC>
2015-01-06 11:34:22 -0500

    Cleaned up message dialog boxes messages all are correct now

--- Chat-Client-Server/src/com/cst242/finalproject/server/ChatServer.java
+++ Chat-Client-Server/src/com/cst242/finalproject/server/ChatServer.java
@@ -13,6 +13,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  * This is the main class for the chat server it has the main() method.
@@ -28,7 +32,7 @@ public class ChatServer {
      * 
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {             
         PrimaryWindow pw = new PrimaryWindow();
         pw.setVisible(true);
         
